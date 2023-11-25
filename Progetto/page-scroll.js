ba4135267ac76ec103b2ed31595236f152d3c961
Akaaiiii <131031299+Akaaiiii@users.noreply.github.com>
2023-11-25 17:47:19 +0100

    Delete Progetto directory

--- Progetto/page-scroll.js
+++
@@ -1,9 +0,0 @@
-let currentSectionIndex = 1;
-const sections = ["intro", "materiali", "collegamenti", "codice"];
-
-function scrollToElement() {
-    const nextSectionId = sections[currentSectionIndex];
-    const nextSectionElement = document.getElementById(nextSectionId);
-    nextSectionElement.scrollIntoView({behavior: "smooth", block: "start"});    
-    currentSectionIndex = (currentSectionIndex + 1) % sections.length;
-}
